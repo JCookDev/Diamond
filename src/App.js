b6d83c44f676e3cad203f685ab650eb8368ca749
Jeff Cook <cookturingedu@gmail.com>
2023-01-12 16:07:15 -0500

    Add styling to app and navbar component's elements

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-//import './App.css';
+import './App.css';
 import { Switch, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 import { Navbar } from './Components';
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button, Menu, Typography, Avatar } from 'antd'; // imports react components from antd llibrary
 import { Link } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
-import icon from '../images/diamondlogo.png'
+import icon from '../images/diamond.png'
 
 
 const Navbar = () => {
@@ -13,7 +13,7 @@ const Navbar = () => {
             <Typography.Title level={2} className="logo"> {/* This line sets the "Diamond Link" on line 14 as an h2*/}
                 <Link to="/">Diamond</Link>
             </Typography.Title>
-            <Button></Button>
+            {/*<Button></Button>*/}
         </div>
     </div>
   )
