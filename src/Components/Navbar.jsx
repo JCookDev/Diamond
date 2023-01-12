742e6c927704c1895d216af8bd40cad08f4141d9
Jeff Cook <cookturingedu@gmail.com>
2023-01-12 14:33:57 -0500

    Add comments to lines of code for clarity

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Menu, Typography, Avatar } from 'antd';
+import { Button, Menu, Typography, Avatar } from 'antd'; // imports react components from antd llibrary
 import { Link } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 import icon from '../images/diamondlogo.png'
@@ -9,8 +9,8 @@ const Navbar = () => {
   return (
     <div className="nav-container">
         <div className="logo-container">
-            <Avatar src={icon} size="large" />
-            <Typography.Title level={2} className="logo">
+            <Avatar src={icon} size="large" /> {/* Use the Avatar Component to define and set the size of the icon*/}
+            <Typography.Title level={2} className="logo"> {/* This line sets the "Diamond Link" on line 14 as an h2*/}
                 <Link to="/">Diamond</Link>
             </Typography.Title>
             <Button></Button>
--- src/Components/index.js
+++ src/Components/index.js
@@ -1,1 +1,3 @@
+// This file exports all JSX Components.
+// Now I can import all of my JSX components in one line; See line 4 in App.js
 export { default as Navbar } from './Navbar';
