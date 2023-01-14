a29b8cbe04754142298553f4d763436de2849bfc
Jeff Cook <cookturingedu@gmail.com>
2023-01-14 11:44:58 -0500

    Configure redux toolkit and fetch data from coinranking api

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,4 +1,6 @@
-const options = {
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+/*const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '84c143d4f2msh3dc3a176c01524ep11d3c9jsnf1dacb849802',
@@ -9,4 +11,28 @@ const options = {
 fetch('https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
-	.catch(err => console.error(err));
+	.catch(err => console.error(err));*/
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://coinranking1.p.rapidapi.com",
+    //prepareHeaders - Callback function that sets the required headers for every API request.
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "84c143d4f2msh3dc3a176c01524ep11d3c9jsnf1dacb849802"
+      );
+
+      return headers;
+    },
+  }),
+  // The code below builds out all of the Coin Ranking API endpoints we're interested in calling.
+  endpoints: (builder) => ({
+    getCryptos: builder.query({ query: () => "/coins" }),
+  }),
+});
+
+export const {
+  useGetCryptosQuery
+} = cryptoApi;
