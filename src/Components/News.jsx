0c85a63bf8675396797326e36eb53585c033385c
Jeff Cook <cookturingedu@gmail.com>
2023-01-15 14:02:08 -0500

    Test cryptoNewsApi endpoint and successfully fetch data

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 
-const News = () => {
+const News = ({ simplified }) => {
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 10 : 100 });
+
+  console.log(cryptoNews)
   return (
     <div>
       News
