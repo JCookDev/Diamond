6f35517243580953c91084f0c62324e4a7eaf607
Jeff Cook <cookturingedu@gmail.com>
2023-01-14 22:33:44 -0500

    Refactor Cryptocurrencies component with useEffect hook for search filter

--- src/Components/Cryptocurrencies.jsx
+++ src/Components/Cryptocurrencies.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import millify from 'millify';
 import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input } from 'antd';
@@ -11,14 +11,26 @@ const Cryptocurrencies = ({ simplified }) => {
   // Destructure the data coming from the API and name it cryptosList
   const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
   // Get the cryptocurrencies in the cryptosList object
-  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+  const [cryptos, setCryptos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
-  console.log(cryptos);
+  //console.log(cryptos);
+  
+  useEffect(() => {
+    const filteredData = cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
+    setCryptos(filteredData)
+  }, [cryptosList, searchTerm]);
 
   if(isFetching) return "Loading...";
 
   return (
     <>
+      {!simplified && (
+        <div className='search-crypto'>
+        <Input placeholder='Search Cryptocurrency' onChange={(e) => setSearchTerm(e.target.value)} />
+      </div>
+      )}
       <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map((currency) => (
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.uuid}>
